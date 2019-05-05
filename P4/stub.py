f889cd33fb42c0867efc490b353e018a34d96905
MikeBao99 <mikebao@college.harvard.edu>
2019-05-04 22:46:42 -0400

    max score 8

--- P4/stub.py
+++ P4/stub.py
@@ -15,12 +15,35 @@ class Learner(object):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
+        self.Q = np.zeros((4,3,3,4,4,2))
+        self.eta = 0.8
+        # monkey location, distance, tree bot, tree top, monkey velocity
 
     def reset(self):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
+        self.Q = np.zeros((4,3,3,4,4,2))
 
+        
+    def cat_hor(self,x):
+        if x < 200:
+            return 0
+        if x < 400:
+            return 1
+        else:
+            return 2
+    
+    def cat_vert(self,x):
+        if x < 100:
+            return 0
+        if x < 200:
+            return 1
+        if x < 300:
+            return 2
+        else:
+            return 3
+    
     def action_callback(self, state):
         '''
         Implement this function to learn things and take actions.
@@ -32,18 +55,61 @@ class Learner(object):
         # You'll need to select and action and return it.
         # Return 0 to swing and 1 to jump.
 
-        new_action = npr.rand() < 0.1
-        new_state  = state
 
-        self.last_action = new_action
-        self.last_state  = new_state
+        try:        
+            m_loc,m_vel,dist,t_bot,t_top= 0,0,0,0,0
+            m_loc = self.cat_vert(self.last_state['monkey']['bot'])
+            if state['monkey']['vel'] > 0:
+                m_vel = 2
+            elif state['monkey']['vel'] > -20:
+                m_vel = 1
+            else:
+                m_vel = 0
+            dist = self.cat_hor(self.last_state['tree']['dist'])
+            t_bot = self.cat_vert(self.last_state['tree']['bot'])
+            t_top = self.cat_vert(self.last_state['tree']['top'])
 
-        return self.last_action
+            m_loc_c,m_vel_C,dist_c,t_bot_c,t_top_c= 0,0,0,0,0
+            m_loc_c = self.cat_vert(state['monkey']['bot'])
+            print(state['monkey']['vel'])
+            if state['monkey']['vel'] > 0:
+                m_vel_c = 2
+            elif state['monkey']['vel'] > -20:
+                m_vel_c = 1
+            else:
+                m_vel_c = 0
+            dist_c = self.cat_hor(state['tree']['dist'])
+            t_bot_c = self.cat_vert(state['tree']['bot'])
+            t_top_c = self.cat_vert(state['tree']['top'])
+
+            self.Q[m_loc,m_vel,dist,t_bot,t_top,self.last_action] -= self.eta*(self.Q[m_loc,m_vel,dist,t_bot,t_top,self.last_action]  - self.last_reward - max(self.Q[m_loc_c,m_vel_c,dist_c,t_bot_c,t_top_c,0] ,self.Q[m_loc_c,m_vel_c,dist_c,t_bot_c,t_top_c,1]))
+            print(self.Q[m_loc,m_vel,dist,t_bot,t_top,self.last_action])
+            if self.Q[m_loc_c,m_vel_c,dist_c,t_bot_c,t_top_c,0] >= self.Q[m_loc_c,m_vel_c,dist_c,t_bot_c,t_top_c,1]:
+                self.last_action = 0
+            else:
+                self.last_action = 1
+
+            if m_loc_c == 0:
+                self.last_action = 1
+          
+          
+            self.last_state  = state
+
+            return self.last_action
+        except Exception as e:
+            print(e)
+            print("holy crap")
+            self.last_state = state
+            self.last_action = 0
+            return 0
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
 
-        self.last_reward = reward
+        if reward < 0:
+            self.last_reward = reward
+        else:
+            self.last_reward = 2
 
 
 def run_games(learner, hist, iters = 100, t_len = 100):
@@ -73,16 +139,16 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 
 if __name__ == '__main__':
 
-	# Select agent.
-	agent = Learner()
+    # Select agent.
+    agent = Learner()
 
-	# Empty list to save history.
-	hist = []
+    # Empty list to save history.
+    hist = []
 
-	# Run games. 
-	run_games(agent, hist, 20, 10)
+    # Run games. 
+    run_games(agent, hist, 50, 10)
 
-	# Save history. 
-	np.save('hist',np.array(hist))
+    # Save history. 
+    np.save('hist',np.array(hist))
 
 
